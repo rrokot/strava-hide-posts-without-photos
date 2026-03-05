c8254300dc36f1fbfd61c826a4c9fc48b3a739a9
rrokot <rrokot@gmail.com>
2026-03-06 00:04:01 +0100

    Add userscript auto-update metadata

--- strava-photo-filter-toggle.meta.js
+++ strava-photo-filter-toggle.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Strava Dashboard - Photo Filter Toggle with Persistence
-// @version      5.0
+// @version      5.7
 // @description  Adds filter buttons to hide posts without photos and/or virtual activities on Strava Dashboard. State persists between reloads.
 // @author       https://www.strava.com/athletes/5931245
 // @match        https://www.strava.com/dashboard*
