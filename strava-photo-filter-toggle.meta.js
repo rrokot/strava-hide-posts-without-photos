0bc4c7d0d189bfdacd74ce20ded8d0af5b451918
rrokot <rrokot@gmail.com>
2026-06-08 00:03:21 +0200

    Update userscript metadata

--- strava-photo-filter-toggle.meta.js
+++ strava-photo-filter-toggle.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Strava Feed Filters
-// @version      5.51
+// @namespace    https://github.com/rrokot/strava-hide-posts-without-photos
+// @version      5.52.0
 // @description  Hide posts without photos or videos, virtual activities, posts you already liked, and your own posts in your Strava feed. Adds a Following/My Activity toggle.
 // @author       https://www.strava.com/athletes/5931245
 // @match        https://www.strava.com/dashboard*
 // @grant        none
+// @run-at       document-idle
 // @icon         https://raw.githubusercontent.com/rrokot/strava-hide-posts-without-photos/main/icon-64.png
 // @updateURL    https://raw.githubusercontent.com/rrokot/strava-hide-posts-without-photos/main/strava-photo-filter-toggle.meta.js
 // @downloadURL  https://raw.githubusercontent.com/rrokot/strava-hide-posts-without-photos/main/strava-photo-filter-toggle.user.js
